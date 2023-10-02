b3b4d72e4fa5434ea0f761f477d84b28bf3318a0
Falme Streamless <Falme@users.noreply.github.com>
2023-10-02 23:58:37 +0000

    Create CTF-File-Checker.sh

---
+++ CTF-File-Checker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z  "$1" ]]; then
+        echo "Not Valid argument"
+        echo "Example: CTF-File-Checker-sh ~/file/to/file.txt"
+        exit 0
+fi
+
+if [[ ! -e "$1" ]]; then
+        echo "File Does not Exist"
+        echo "Example: CTF-File-Checker-sh ~/file/to/file.txt"
+        exit 0
+fi
+
+flagKey="flag{"
+
+
+if [[ ! -z "$2" ]]; then
+        flagKey="$2"
+fi
+
+
+
+strings "$1" | grep "${flagKey}" 
+
+zbarimg "$1" | grep "${flagKey}"
