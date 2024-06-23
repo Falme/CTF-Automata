1ba4b4c32cd4441e9312ca77d1c9265d8ac45b7b
Falme Streamless <Falme@users.noreply.github.com>
2024-06-23 05:55:19 +0000

    Created Python script to find flag in a page source code

---
+++ ctf.py
@@ -0,0 +1,31 @@
+import sys
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import re
+
+
+print("==== Falme's CTF Automata ====\n")
+print("Starting Application...\n\n")
+
+if (len(sys.argv) <= 2):
+    print("ERROR: Not Enough Arguments for the operation\n")
+    print("Usage:")
+    print("ctf.py <HttpAddress> <flagID>")
+else:
+    print("Looking for flags in address...")
+
+    url = sys.argv[1]
+    flagID = sys.argv[2]
+
+    driver = webdriver.Firefox()
+    
+    driver.get(url)
+    time.sleep(2)
+    
+    content = driver.page_source
+    print("Flags Found in Page Source:")
+    print(re.findall(flagID+"\{[^}]*\}",content, re.DOTALL))
+
+    driver.close()
