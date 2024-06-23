2970444e379ab88f9a1ac570cb3db5bae363d136
Falme Streamless <falme_streamless@hotmail.com>
2024-06-23 13:52:04 -0400

    Created Section to read File Strings

--- ctf.py
+++ ctf.py
@@ -1,9 +1,10 @@
 import sys
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import re
+import validators
+import subprocess
 
 
 print("==============================")
@@ -14,20 +15,39 @@ print("Starting Application...\n\n")
 if (len(sys.argv) <= 2):
     print("ERROR: Not Enough Arguments for the operation\n")
     print("Usage:")
-    print("ctf.py <HttpAddress> <flagID>")
+    print("ctf.py <WebURLAddress or PathAddress> <flagID>")
 else:
-    print("Looking for flags in address...")
 
-    url = sys.argv[1]
+    address = sys.argv[1]
     flagID = sys.argv[2]
 
-    driver = webdriver.Firefox()
-    
-    driver.get(url)
-    time.sleep(2)
-    
-    content = driver.page_source
-    print("Flags Found in Page Source:")
-    print(re.findall(flagID+"\{[^}]*\}",content, re.DOTALL))
+    validation = validators.url(address)
 
-    driver.close()
+    if validation:
+        print("Address is an URL...")
+        print("Looking for flags in Address...")
+
+        driver = webdriver.Firefox()
+        
+        driver.get(address)
+        time.sleep(2)
+        
+        content = driver.page_source
+        print("Flags Found in Page Source:")
+        print(re.findall(flagID+"\{[^}]*\}",content, re.DOTALL))
+
+        driver.close()
+    else:
+        print("Address is not an URL...")
+        print("Trying File in System...")
+        print("")
+
+        print("File Command:")
+        print(subprocess.run(["file", address], capture_output=True, text=True))
+
+        print("")
+        print("Strings Command:")
+        file_strings = str(subprocess.run(["strings", address], capture_output=True, text=True))
+        print(re.findall(flagID+"\{[^}]*\}",file_strings, re.DOTALL))
+
+
